b037274a3e674f1d64539af43314e7fc67334842
Luca de Montis <luca.demo@hotmail.it>
2018-05-29 11:10:14 +0200

    Almost done with middleman

--- config.rb
+++ config.rb
@@ -20,3 +20,8 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+#added to iterate over names in file yaml to create new pages
+["papillard", "ssaunier", "monsieurpaillard"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", locals: { owner: name }, ignore: true
+end
